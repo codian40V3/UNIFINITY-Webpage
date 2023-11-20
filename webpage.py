2d83a33a5adcc53b4bc43bdc1e8c6242e70bd424
Codian <jusatsit@gmail.com>
2023-11-20 15:17:30 +0100

    commit favicon

--- webpage.py
+++ webpage.py
@@ -1,11 +1,18 @@
-from flask import Flask
+import os
+from flask import Flask, render_template, send_from_directory
 
 app = Flask(__name__)
 
 
 @app.route("/")
-def hello_world():
-    return "<p>This will be the UNIFINITY Website</p>"
+def home():
+    return render_template("home.html")
+
+
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'),
+                               'favicon.ico', mimetype='image/vnd.microsoft.icon')
 
 
 if __name__ == "__main__":
