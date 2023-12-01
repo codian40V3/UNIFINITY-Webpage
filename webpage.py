3f614ab6db99365a22e645b03dbf712b063aa11d
Codian <jusatsit@gmail.com>
2023-12-01 17:54:38 +0100

    added requirements.txt

--- webpage.py
+++ webpage.py
@@ -9,6 +9,10 @@ def home():
     return render_template("home.html")
 
 
+@app.route("/scratch")
+def scratch():
+    return render_template("scratch.html")
+
 @app.route('/favicon.ico')
 def favicon():
     return send_from_directory(os.path.join(app.root_path, 'static'),
