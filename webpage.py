dcb0b56491c9c19f4786f49c09d7fecf19c9d47c
Codian <jusatsit@gmail.com>
2023-12-15 13:54:34 +0100

    UNIFINITY WEBSITE 1.0

--- webpage.py
+++ webpage.py
@@ -1,8 +1,12 @@
 import os
 from flask import Flask, render_template, send_from_directory, request
 from flask_mail import Mail, Message
-from config import sender_mail_username, sender_mail_password, recipient_mail_username
 
+from dotenv import dotenv_values
+
+
+config = dotenv_values(".env")
+config_list = [config]
 
 app = Flask(__name__)
 
@@ -10,8 +14,8 @@ app.config["MAIL_SERVER"] = "smtp.gmail.com"
 app.config["MAIL_PORT"] = 465
 app.config["MAIL_USE_TLS"] = False
 app.config["MAIL_USE_SSL"] = True
-app.config["MAIL_USERNAME"] = sender_mail_username
-app.config["MAIL_PASSWORD"] = sender_mail_password
+app.config["MAIL_USERNAME"] = config["sender_mail_username"]
+app.config["MAIL_PASSWORD"] = config["sender_mail_password"]
 
 
 mail = Mail(app)
@@ -22,16 +26,17 @@ def home():
     return render_template("home.html")
 
 
-@app.route("/contact", methods=["GET", "POST"])
+@app.route("/sent", methods=["GET", "POST"])
 def contact():
     if request.method == "POST":
         email = request.form.get("email")
         subject = request.form.get("subject")
         message = request.form.get("message")
 
-        msg = Message(sender=sender_mail_username, recipients=recipient_mail_username,
-                      subject="UNIFINITY Contact Form submission", body=f"Email: {email}\nSubject: {subject}"
-                                                                        f"\n\n{message}")
+        msg = Message(sender=config.get("sender_mail_username"),
+                      recipients=[config.get("recipient_mail_username")],
+                      subject="UNIFINITY Contact Form submission",
+                      body=f"Email: {email}\nSubject: {subject}\n\n{message}")
         mail.send(msg)
     return render_template("contact.html")
 
