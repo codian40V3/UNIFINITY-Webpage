3e236a30561e987fb1519b6150a4bf1bebbecd75
Codian <jusatsit@gmail.com>
2023-12-18 16:49:14 +0100

    UNIFINITY WEBSITE 1.6.1 Brevo SMTP

--- webpage.py
+++ webpage.py
@@ -9,7 +9,7 @@ config_list = [config]
 
 app = Flask(__name__)
 
-app.config["MAIL_SERVER"] = "smtp-mail.outlook.com"
+app.config["MAIL_SERVER"] = "smtp-relay.brevo.com"
 app.config["MAIL_PORT"] = 587
 app.config["MAIL_USE_TLS"] = True
 app.config["MAIL_USE_SSL"] = False
