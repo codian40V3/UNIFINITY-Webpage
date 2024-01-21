c43bcd7a584e47b65153e25da8dc76cd37682d9d
Codian <jusatsit@gmail.com>
2024-01-21 15:10:08 +0100

    UNIFINITY WEBSITE 1.8.3 SMTP2Go add Username

--- webpage.py
+++ webpage.py
@@ -12,6 +12,7 @@ app.config["MAIL_SERVER"] = "mail.smtp2go.com"
 app.config["MAIL_PORT"] = 2525
 app.config["MAIL_USE_TLS"] = True
 app.config["MAIL_USE_SSL"] = False
+app.config['MAIL_USERNAME'] = config.get("unifinity_sender_username")
 
 mail = Mail(app)
 
